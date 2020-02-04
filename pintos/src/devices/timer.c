8b5a26348fcd27e73ace97830dad82ff8133724b
Bethany Weddle <bnweddle@cougar.cs.ksu.edu>
2020-02-04 13:00:57 -0600

    initialized start to timer_ticks

--- pintos/src/devices/timer.c
+++ pintos/src/devices/timer.c
@@ -89,6 +89,7 @@ timer_elapsed (int64_t then)
 void
 timer_sleep (int64_t ticks) 
 {
+  int64_t start = timer_ticks();
   int64_t wakeup_at = start + ticks;
 
   ASSERT (intr_get_level () == INTR_ON);
