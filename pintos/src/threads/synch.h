f483d82c6c35a39525764324b087f6536df7776b
Mir Anwar <fahimanwar@cougar.cs.ksu.edu>
2020-02-06 12:55:19 -0600

    modified synch.c

--- pintos/src/threads/synch.h
+++ pintos/src/threads/synch.h
@@ -22,6 +22,8 @@ struct lock
   {
     struct thread *holder;      /* Thread holding lock (for debugging). */
     struct semaphore semaphore; /* Binary semaphore controlling access. */
+    struct list_elem elem_lock;
+    int priority_lock;
   };
 
 void lock_init (struct lock *);
@@ -40,7 +42,10 @@ void cond_init (struct condition *);
 void cond_wait (struct condition *, struct lock *);
 void cond_signal (struct condition *, struct lock *);
 void cond_broadcast (struct condition *, struct lock *);
-
+bool sema_compare( struct list_elem *,struct list_elem *,void *);
+static bool lock_priority_more ( struct list_elem *,
+                                 struct list_elem *,
+                                void *);
 /* Optimization barrier.
 
    The compiler will not reorder operations across an
